43c8d27cf7d4c182c6bd3549303fc98f218f211b
unknown <shakibahmed09112017@gmail.com>
2022-08-21 01:52:05 -0700

    first commit

---
+++ src/main/java/com/example/dev/springbootdevspringbasicconcepts/SpringBootDevSpringBasicConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev.springbootdevspringbasicconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevSpringBasicConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevSpringBasicConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dev/springbootdevspringbasicconcepts/SpringBootDevSpringBasicConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev.springbootdevspringbasicconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevSpringBasicConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
